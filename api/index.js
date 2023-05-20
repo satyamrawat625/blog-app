eb400eb674c4be642901233087c497b1866f1299
Satyam Rawat <78161825+satyamrawat625@users.noreply.github.com>
2023-05-20 22:30:41 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -82,6 +82,11 @@ app.post('/register', async (req,res) => {
 app.post('/login', async (req,res) => {
   const {username,password} = req.body;
   const userDoc = await User.findOne({username});
+  
+    if(!userDoc){
+    res.status(400).json('Wrong credentials');
+    return ;
+  }
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
